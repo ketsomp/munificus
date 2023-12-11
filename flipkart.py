34137a023c7cf91f336a3a0369cd454991567515
Aniket <aniketpruthvi@gmail.com>
2023-12-11 16:25:01 +0530

    remarks

--- flipkart.py
+++ flipkart.py
@@ -9,16 +9,21 @@ def flipkart(input):
     soup=bs(page.content,'html.parser')
     data=soup.find_all('div',class_='_1AtVbE col-12-12')
     results=[]
+    if page.status_code==200:
+        print("Flipkart connected")
+    else:
+        print("Failed to connect to Flipkart:",page.status_code)
     for i in data:
         try:
             name=i.find('div',class_='_4rR01T').get_text()
             rating=i.find('div',class_="_3LWZlK").get_text()
             price=i.find('div',class_="_30jeq3").get_text()
             n=name.split(' ')[:8]
-            l=[''.join(x+' ' for x in n),price,rating,None]
+            l=['1',''.join(x+' ' for x in n),price,rating,None]
             results.append(l)
         except:
-            return None
+            print('Flipkart Fetch Failed!')
+            break
 
 
     return results
