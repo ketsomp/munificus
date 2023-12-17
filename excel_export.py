44dd38008073317ddbcf770d332981f44a566855
Aniket <aniketpruthvi@gmail.com>
2023-12-17 13:27:06 +0530

    edited inputs

--- excel_export.py
+++ excel_export.py
@@ -1,6 +1,6 @@
 import xlsxwriter
 
-def write_product_data_to_excel(amazon_data, myntra_data, file_name):
+def write_product_data_to_excel(amazon_data, flip_data, file_name):
     # Workbook and Worksheet
     workbook = xlsxwriter.Workbook(file_name)
     worksheet = workbook.add_worksheet()
@@ -9,12 +9,13 @@ def write_product_data_to_excel(amazon_data, myntra_data, file_name):
     headers = ["Product", "Price", "Rating"]
     for col_num, header in enumerate(headers):
         worksheet.write(0, col_num, header)
+    print(amazon_data)
 
     # Track min/max vals
     min_amazon_price = float("inf")
     max_amazon_rating = -float("inf")
-    min_myntra_price = float("inf")
-    max_myntra_rating = -float("inf")
+    min_flip_price = float("inf")
+    max_flip_rating = -float("inf")
 
     # Amazon Data
     for row_num, item in enumerate(amazon_data, start=1):
@@ -50,40 +51,40 @@ def write_product_data_to_excel(amazon_data, myntra_data, file_name):
         workbook.add_format({"bold": True}),
     )
 
-    # Myntra Data
-    for row_num, item in enumerate(myntra_data, start=len(amazon_data) + 4):
+    # flip Data
+    for row_num, item in enumerate(flip_data, start=len(amazon_data) + 4):
         # Track min/max
-        price = float(item[1])
+        price = float(''.join(str(item) for item in item[1].split(',')))
         rating = float(item[2].split()[0])
 
-        if price < min_myntra_price:
-            min_myntra_price = price
-        if rating > max_myntra_rating:
-            max_myntra_rating = rating
+        if price < min_flip_price:
+            min_flip_price = price
+        if rating > max_flip_rating:
+            max_flip_rating = rating
 
         # Write row
         for col_num in range(len(headers)):
             cell_format = None
-            if (col_num == 1 and price == min_myntra_price) or (
-                col_num == 2 and rating == max_myntra_rating
+            if (col_num == 1 and price == min_flip_price) or (
+                col_num == 2 and rating == max_flip_rating
             ):
                 cell_format = workbook.add_format({"bg_color": "yellow"})
             worksheet.write(row_num, col_num, item[col_num], cell_format)
 
-    # Myntra Header
+    # flip Header
     worksheet.merge_range(
-        len(amazon_data) + len(myntra_data) + 5,
+        len(amazon_data) + len(flip_data) + 5,
         0,
-        len(amazon_data) + len(myntra_data) + 5,
+        len(amazon_data) + len(flip_data) + 5,
         2,
-        "Myntra",
+        "flip",
         workbook.add_format({"bold": True}),
     )
 
     workbook.close()
     print("Excel file written successfully!")
 
-if __name__ == '__main__':
-    amazon_data = [["Wireless Mouse A", "599", "4.3 stars"], ["Wireless Mouse B", "595", "3.6 stars"]]
-    myntra_data = [["Wireless Mouse X", "590", "4.5 stars"], ["Wireless Mouse Y", "595", "4.2 stars"]]
-    write_product_data_to_excel(amazon_data, myntra_data, "output_file.xlsx")
+# if __name__ == '__main__':
+#     amazon_data = [["Wireless Mouse A", "599", "4.3 stars",None], ["Wireless Mouse B", "595", "3.6 stars",None]]
+#     flip_data = [["Wireless Mouse X", "590", "4.5 stars",None], ["Wireless Mouse Y", "595", "4.2 stars",None]]
+#     write_product_data_to_excel(amazon_data, flip_data, "output_file.xlsx")
