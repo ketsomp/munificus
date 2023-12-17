afeefd71eed7b65d24690e865134223a38772164
Aniket <aniketpruthvi@gmail.com>
2023-12-17 13:27:59 +0530

    removed rupee sign

--- flipkart.py
+++ flipkart.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup as bs
 import requests
 
@@ -9,7 +10,7 @@ def flipkart(input):
     data=soup.find_all('div',class_='_1AtVbE col-12-12')
     results=[]
     if page.status_code==200:
-        print("Flipkart connected!")
+        print("Flipkart connected")
     else:
         print("Failed to connect to Flipkart:",page.status_code)
     for i in data:
@@ -17,13 +18,8 @@ def flipkart(input):
             name=i.find('div',class_='_4rR01T').get_text()
             rating=i.find('div',class_="_3LWZlK").get_text()
             price=i.find('div',class_="_30jeq3").get_text()
-            # if i.find('div',class_='_2kHMtA'):
-            #     b=i.find('div',class_='_2kHMtA')
-            #     c=b.find('a')['href']
-            #     if c:
-            #         link=c.get_text()
             n=name.split(' ')[:8]
-            l=[1,''.join(x+' ' for x in n),price,rating,None]
+            l=[1,''.join(x+' ' for x in n),price[1:],rating,None]
             results.append(l)
         except:
             print('Flipkart Fetch Failed!')
@@ -33,4 +29,3 @@ def flipkart(input):
 
 #    product_name = soup.find("span",{"class":"B_NuCI"}).get_text()
 # <span class="B_NuCI">
-# print(flipkart('laptop'))
