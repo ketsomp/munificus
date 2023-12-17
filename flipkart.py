00f72e37c2d2809c1ed9dee9dc4cd49be83fe52e
Aniket <aniketpruthvi@gmail.com>
2023-12-17 12:42:33 +0530

    key error resolved

--- flipkart.py
+++ flipkart.py
@@ -1,4 +1,3 @@
-import bs4
 from bs4 import BeautifulSoup as bs
 import requests
 
@@ -10,7 +9,7 @@ def flipkart(input):
     data=soup.find_all('div',class_='_1AtVbE col-12-12')
     results=[]
     if page.status_code==200:
-        print("Flipkart connected")
+        print("Flipkart connected!")
     else:
         print("Failed to connect to Flipkart:",page.status_code)
     for i in data:
@@ -18,16 +17,20 @@ def flipkart(input):
             name=i.find('div',class_='_4rR01T').get_text()
             rating=i.find('div',class_="_3LWZlK").get_text()
             price=i.find('div',class_="_30jeq3").get_text()
+            # if i.find('div',class_='_2kHMtA'):
+            #     b=i.find('div',class_='_2kHMtA')
+            #     c=b.find('a')['href']
+            #     if c:
+            #         link=c.get_text()
             n=name.split(' ')[:8]
-            l=['1',''.join(x+' ' for x in n),price,rating,None]
+            l=[1,''.join(x+' ' for x in n),price,rating,None]
             results.append(l)
         except:
             print('Flipkart Fetch Failed!')
-            break
 
 
     return results
 
 #    product_name = soup.find("span",{"class":"B_NuCI"}).get_text()
 # <span class="B_NuCI">
-print(flipkart('laptop'))
+# print(flipkart('laptop'))
