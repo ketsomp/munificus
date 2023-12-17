7fb6184c10246afff0dc3869ebdb87f143d3edb9
Aniket <aniketpruthvi@gmail.com>
2023-12-17 23:25:03 +0530

    electronics and groceries toggle in hamburger, random and datetime

--- main.py
+++ main.py
@@ -7,23 +7,43 @@ import math
 from test import test,ttest
 from flipkart import flipkart
 from amazon import amazon
+from bigbasket import bigbasket
 from excel_export import write_product_data_to_excel
 from tkinter import messagebox
+import random
+import datetime
 
 root = ctk.CTk()
 root.geometry('1200x800')
 root.title('Munificus')
 root.resizable(0,0)
 
+toggle_counter=1
+
 def ham():
+    global toggle_button
     printer_img=ctk.CTkImage(light_image=Image.open('assets/printer.png'),
                         size=(32,32))
     printer_button=ctk.CTkButton(root,image=printer_img,text='',width=32,command=csv)
     printer_button.place(x=10,y=60)
+    toggle_button=ctk.CTkButton(root,image=elec_img,text='',width=32,command=toggle)
+    toggle_button.place(x=10,y=110)
+
 
 def csv():
-    write_product_data_to_excel([element[1:] for element in amzn], [element[1:] for element in flip], "output_file.xlsx")
-    messagebox.showinfo('Success!','Written successfully!\nRefer to output_file.csv')
+    try:
+        write_product_data_to_excel([element[1:] for element in amzn], [element[1:] for element in flip], "output_file.xlsx")
+        messagebox.showinfo('Success!',f'Written successfully!\n\nBest Price:{results[0][1]}\n\nBest Rating:{sorted(results,key=lambda x:x[3])[0][1]}\n\nRefer to output_file.csv for further details')
+    except:
+        messagebox.showinfo('Error','Please run a query first!')
+
+def toggle():
+    global toggle_counter
+    toggle_counter*=-1
+    if toggle_counter==1:
+        toggle_button.configure(image=elec_img)
+    elif toggle_counter==-1:
+        toggle_button.configure(image=groc_img)
 
 
 def rating_stars(n,c):
@@ -43,14 +63,15 @@ def rating_stars(n,c):
         print('plac')
 
 def get_input():
-    global amzn,flip
+    global amzn,flip,results
     # web scraping file call goes here
-    input='laptop' if item_entry.get() is None else item_entry.get()
+    input=random.choice(random_default) if item_entry.get() is '' else item_entry.get()
     amzn=amazon(input)[0:6]
     flip=flipkart(input)[0:6]
+    big=bigbasket(input)[0:6]
     # flip=test('lal')
     # amzn=ttest('lal')
-    results=flip+amzn
+    results=flip+amzn if toggle_counter==1 else big
     results=sorted(results,key=lambda x:x[2])
 
     for i in range(len(companies)):
@@ -69,10 +90,11 @@ def get_input():
 photos=[]
 names=[]
 prices=[]
-delivery_date=[]
+delivery_date=[] 
 rating=[]
+random_default=['laptop','mobile','television','iphone 14','samsung s23']
 
-companies={0:'Amazon',1:'Flipkart',2:'Ebay',3:'Myntra'}
+companies={0:'Amazon',1:'Flipkart',2:'BigBasket',3:'Myntra'}
 
 logo_img = ImageTk.PhotoImage(Image.open("assets/properdark munificus.png").resize((200, 200)))
 logo_label = Label(root, image=logo_img)
@@ -89,6 +111,14 @@ search_button.place(x=777,y=256)
 
 item_entry=ctk.CTkEntry(root,width=335,height=40)
 item_entry.place(x=433,y=255)
+
+date_button=ctk.CTkButton(root,text=str(datetime.date.today()),width=32)
+date_button.place(x=1100,y=10)
+
+elec_img=ctk.CTkImage(light_image=Image.open('assets/electronics.png'),
+                        size=(32,32))
+groc_img=ctk.CTkImage(light_image=Image.open('assets/groceries.png'),
+                        size=(32,32))
 
 item_button=Button(root,width=100,text='Enter',command=get_input) # use ctk.CtkButton at the end, 
 # ctk glitches out on mac so using ttk button for testing
